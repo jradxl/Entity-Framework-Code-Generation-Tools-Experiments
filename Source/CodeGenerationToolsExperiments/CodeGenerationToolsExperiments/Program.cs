015c4369bdf5acfacee3128fdbe117600d27a05a
John R <jrad@cix.co.uk>
2013-06-05 21:00:54 +0100

    Updated Console Program with more examples

--- Source/CodeGenerationToolsExperiments/CodeGenerationToolsExperiments/Program.cs
+++ Source/CodeGenerationToolsExperiments/CodeGenerationToolsExperiments/Program.cs
@@ -2,6 +2,7 @@
 using EFUtility.CodeGenerationTools;
 using Microsoft.VisualStudio.TextTemplating;
 using System;
+using System.Collections.Generic;
 using System.Data.Mapping;
 using System.Data.Metadata.Edm;
 using System.IO;
@@ -14,8 +15,13 @@ namespace CodeGenerationToolsExperiments
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Entity Frameworks CodeGenerationTools Demo Program.\n");
+
+            //I had problems with accessing Dictionary by [] index, so quickly used these
+            //to check in normal C# usage.
+            //var tc = new TestCollections();
+            //tc.TestCollections2();
+
             try
             {
                 //As this program only reads and interprets the XML within the EDMX,
@@ -44,6 +50,90 @@ namespace CodeGenerationToolsExperiments
                 EF5FluentUtility efu = new EF5FluentUtility();
                 MetadataLoadResult mlr = efu.LoadMetadata(inputFile);
 
+                var a = mlr.EdmItems;
+                var b = mlr.ManyToManyMappings;
+                var c = mlr.PropertyToColumnMapping;
+                var d = mlr.TphMappings;
+
+                //Obtain the EntityContainer from the ItemsCollection
+                var container = mlr.EdmItems.GetItems<EntityContainer>()[0];
+
+                //Iterate all the EntitySets and Properties
+                //EntitySet are as in "People", where the EntityType is "Person"
+                foreach (var entityset in container.BaseEntitySets.OfType<EntitySet>().OrderBy(e => e.Name))
+                {
+                    var entitysetname = entityset.Name;
+                    var entityname = entityset.ElementType.Name;
+                    var entitytype = entityset.ElementType;
+                    var declaringtype = entityset.ElementType.NavigationProperties[0].DeclaringType;
+
+                    //Navigation Properties for a particular Entity
+                    var collectionNavigations1 = entityset.ElementType.NavigationProperties.Where(np => np.DeclaringType == entityset.ElementType);
+
+                    // Find m:m relationshipsto configure for a particular Entity
+                    var manyManyRelationships = entityset.ElementType.NavigationProperties
+                        .Where(np =>
+                            //np.DeclaringType is EntityType &&
+                            np.ToEndMember.RelationshipMultiplicity == RelationshipMultiplicity.Many
+                            && np.FromEndMember.RelationshipMultiplicity == RelationshipMultiplicity.Many
+                                // Ensures we only configure from one end.
+                                // Convention is to have source on the left, as it would be diagrammatically.
+                            && np.RelationshipType.RelationshipEndMembers.First() == np.FromEndMember)
+                        .ToArray();
+
+                    //Now process the M:Ms
+                    foreach (var navProperty in manyManyRelationships)
+                    {
+                        var otherNavProperty = navProperty.ToEndMember.GetEntityType().NavigationProperties.Where(n => n.RelationshipType == navProperty.RelationshipType && n != navProperty).Single();
+                        var association = (AssociationType)navProperty.RelationshipType;
+
+                        //This did not work in the T4 Template.
+                        var mapping1 = mlr.ManyToManyMappings[association];
+
+                        var mapping2 = mlr.ManyToManyMappings.Where(m => m.Key.Name == association.Name).FirstOrDefault().Value;
+                        var item1 = mapping1.Item1;
+
+                        //This did not work in the T4 Template.
+                        var leftKeyMappings1 = mapping1.Item2[navProperty.ToEndMember];
+                        var leftKeyMappings2 = mapping1.Item2.Where(np => np.Key.Name == navProperty.ToEndMember.Name).FirstOrDefault().Value;
+
+                        // Need to ensure that FKs are declared in the same order as the PK properties on each principal type
+                        var leftType = (EntityType)navProperty.DeclaringType;
+                        var rightType = (EntityType)otherNavProperty.DeclaringType;
+
+                        //Access using Index [navProperty.FromEndMember] did not work within template.
+                        var leftKeyMappings = mapping1.Item2.Where(np => np.Key.Name == navProperty.FromEndMember.Name).FirstOrDefault().Value; //[navProperty.FromEndMember];
+                        var rightKeyMappings = mapping1.Item2.Where(np => np.Key.Name == otherNavProperty.FromEndMember.Name).FirstOrDefault().Value; //[otherNavProperty.FromEndMember];
+
+                        var left = leftKeyMappings.Where(km => km.Key.Name == "PersonID");
+                        var right = rightType.KeyMembers.Select(m => "\"" + rightKeyMappings.Where(km => km.Key.Name == m.Name).FirstOrDefault().Value + "\"").FirstOrDefault();
+                    }
+
+                    //Dual OrderBy to bring Keys to top of list
+                    //Iterate all properties for this particular Entity.
+                    var properties = entityset.ElementType.Properties.OrderBy(s => s.Name).OrderBy(p => entityset.ElementType.KeyMembers.Contains(p) == false).ToArray();
+                    foreach (var property in properties)
+                    {
+                        var propertyname = property.Name;
+
+                        //This did not work in the T4 Template.
+                        var mapping1 = mlr.PropertyToColumnMapping[entityset.ElementType];
+
+                        //PropertyToColumnMapping is a collection for ALL Entities, so here the particular Entity needs to be filtered out.
+                        //Mapping returned is a complex generic Dictionary.
+                        var mapping2 = mlr.PropertyToColumnMapping.Where(k => k.Key == entityset.ElementType).FirstOrDefault().Value;
+                        var mapping3 = mlr.PropertyToColumnMapping.Where(k => k.Key.FullName == entityset.ElementType.FullName).FirstOrDefault().Value;
+
+                        var mapping4 = mapping3.Item2.Where(p => p.Key.Name == property.Name).FirstOrDefault().Value;
+
+                        var i1 = mapping1.Item1;
+                        var i2 = mapping1.Item2;
+                        var mapright = i2[property];
+                    }
+
+                }
+
+                //********************************************************************************
 
                 //Demo 2
                 //This takes EF.Utility.CS.ttinclude and receates as
@@ -139,6 +229,186 @@ namespace CodeGenerationToolsExperiments
             XElement csdl = root.Descendants(XName.Get("ConceptualModels", edmxNS)).FirstOrDefault();
 
             return csdl;
+        }
+    }
+
+    public class TestCollections
+    {
+        public void TestCollections1()
+        {
+            List<string> dinosaurs = new List<string>();
+
+            Console.WriteLine("\nCapacity: {0}", dinosaurs.Capacity);
+
+            dinosaurs.Add("Tyrannosaurus");
+            dinosaurs.Add("Amargasaurus");
+            dinosaurs.Add("Mamenchisaurus");
+            dinosaurs.Add("Deinonychus");
+            dinosaurs.Add("Compsognathus");
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            Console.WriteLine("\nCapacity: {0}", dinosaurs.Capacity);
+            Console.WriteLine("Count: {0}", dinosaurs.Count);
+
+            var third = dinosaurs[2];
+
+            Console.WriteLine("\nContains(\"Deinonychus\"): {0}",
+                dinosaurs.Contains("Deinonychus"));
+
+            Console.WriteLine("\nInsert(2, \"Compsognathus\")");
+            dinosaurs.Insert(2, "Compsognathus");
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            Console.WriteLine("\ndinosaurs[3]: {0}", dinosaurs[3]);
+
+            Console.WriteLine("\nRemove(\"Compsognathus\")");
+            dinosaurs.Remove("Compsognathus");
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            dinosaurs.TrimExcess();
+            Console.WriteLine("\nTrimExcess()");
+            Console.WriteLine("Capacity: {0}", dinosaurs.Capacity);
+            Console.WriteLine("Count: {0}", dinosaurs.Count);
+
+            dinosaurs.Clear();
+            Console.WriteLine("\nClear()");
+            Console.WriteLine("Capacity: {0}", dinosaurs.Capacity);
+            Console.WriteLine("Count: {0}", dinosaurs.Count);
+        }
+
+        public void TestCollections2()
+        {
+            // Create a new dictionary of strings, with string keys. 
+            //
+            Dictionary<string, string> openWith = new Dictionary<string, string>();
+
+            // Add some elements to the dictionary. There are no  
+            // duplicate keys, but some of the values are duplicates.
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+
+            var key = "xrtf";
+            //var third = openWith[key];
+            var third2 = openWith.Where(k => k.Key == key).FirstOrDefault().Value;
+
+            // The Add method throws an exception if the new key is  
+            // already in the dictionary. 
+            try
+            {
+                openWith.Add("txt", "winword.exe");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with Key = \"txt\" already exists.");
+            }
+
+            // The Item property is another name for the indexer, so you  
+            // can omit its name when accessing elements. 
+            Console.WriteLine("For key = \"rtf\", value = {0}.",
+                openWith["rtf"]);
+
+            // The indexer can be used to change the value associated 
+            // with a key.
+            openWith["rtf"] = "winword.exe";
+            Console.WriteLine("For key = \"rtf\", value = {0}.",
+                openWith["rtf"]);
+
+            // If a key does not exist, setting the indexer for that key 
+            // adds a new key/value pair.
+            openWith["doc"] = "winword.exe";
+
+            // The indexer throws an exception if the requested key is 
+            // not in the dictionary. 
+            try
+            {
+                Console.WriteLine("For key = \"tif\", value = {0}.",
+                    openWith["tif"]);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key = \"tif\" is not found.");
+            }
+
+            // When a program often has to try keys that turn out not to 
+            // be in the dictionary, TryGetValue can be a more efficient  
+            // way to retrieve values. 
+            string value = "";
+            if (openWith.TryGetValue("tif", out value))
+            {
+                Console.WriteLine("For key = \"tif\", value = {0}.", value);
+            }
+            else
+            {
+                Console.WriteLine("Key = \"tif\" is not found.");
+            }
+
+            // ContainsKey can be used to test keys before inserting  
+            // them. 
+            if (!openWith.ContainsKey("ht"))
+            {
+                openWith.Add("ht", "hypertrm.exe");
+                Console.WriteLine("Value added for key = \"ht\": {0}",
+                    openWith["ht"]);
+            }
+
+            // When you use foreach to enumerate dictionary elements, 
+            // the elements are retrieved as KeyValuePair objects.
+            Console.WriteLine();
+            foreach (KeyValuePair<string, string> kvp in openWith)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
+
+            // To get the values alone, use the Values property.
+            Dictionary<string, string>.ValueCollection valueColl =
+                openWith.Values;
+
+            // The elements of the ValueCollection are strongly typed 
+            // with the type that was specified for dictionary values.
+            Console.WriteLine();
+            foreach (string s in valueColl)
+            {
+                Console.WriteLine("Value = {0}", s);
+            }
+
+            // To get the keys alone, use the Keys property.
+            Dictionary<string, string>.KeyCollection keyColl =
+                openWith.Keys;
+
+            // The elements of the KeyCollection are strongly typed 
+            // with the type that was specified for dictionary keys.
+            Console.WriteLine();
+            foreach (string s in keyColl)
+            {
+                Console.WriteLine("Key = {0}", s);
+            }
+
+            // Use the Remove method to remove a key/value pair.
+            Console.WriteLine("\nRemove(\"doc\")");
+            openWith.Remove("doc");
+
+            if (!openWith.ContainsKey("doc"))
+            {
+                Console.WriteLine("Key \"doc\" is not found.");
+            }
         }
     }
 }
